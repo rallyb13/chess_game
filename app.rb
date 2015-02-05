c3e9481762a998762529d08040cbe6155b323ac3
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-05 14:36:38 -0800

    sinatra form

--- app.rb
+++ app.rb
@@ -3,7 +3,16 @@ Bundler.require :default
 Dir[File.dirname(__FILE__) + "/lib/*.rb"].each { |file| require file }
 
 get '/' do
-  Game.create
-  @oneone = Piece.render(1, 1)
+  @game = Game.create
+  @black_pieces = Piece.all.where(white: false)
+  @white_pieces = Piece.all.where(white: true)
   erb :index
+end
+
+post '/' do
+  x_value = params.fetch('x_value').to_i
+  y_value = params.fetch('y_value').to_i
+  piece = Piece.find(params.fetch('piece').to_i)
+  piece.move(x_value, y_value)
+  redirect '/'
 end
