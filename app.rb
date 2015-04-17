8d158ea6ccae0d1a1c13ac810b8242f65c91b67a
Jackie Fletcher <jaxskriv@gmail.com>
2015-04-17 11:15:45 -0700

    change post game route to try to solve heroku errror of new game initiated by another user, should now just refresh to new game board instead of internal server error

--- app.rb
+++ app.rb
@@ -22,17 +22,20 @@ end
 post '/game' do
   x_value = params.fetch('x_value').to_i
   y_value = params.fetch('y_value').to_i
-  piece = Piece.find(params.fetch('piece').to_i)
-  if piece.move?(x_value, y_value)
-    piece.move_it(x_value,y_value)
-# if piece.promotion?
-    Game.first.turn
-    redirect '/game'
+  if piece = Piece.find(params.fetch('piece').to_i)
+    if piece.move?(x_value, y_value)
+      piece.move_it(x_value,y_value)
+  # if piece.promotion?
+      Game.first.turn
+      redirect '/game'
+    else
+      @message = "Invalid move! Try again."
+      @game = Game.first
+      @black_pieces = Piece.all.where(white: false)
+      @white_pieces = Piece.all.where(white: true)
+      erb :game
+    end
   else
-    @message = "Invalid move! Try again."
-    @game = Game.first
-    @black_pieces = Piece.all.where(white: false)
-    @white_pieces = Piece.all.where(white: true)
-    erb :game
+    redirect '/game'
   end
 end
